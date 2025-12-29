9f69ec74447c07600af7f6c0deef4f7e74f493d6
Davi Amorim <davi.amorim@souodonto.com>
2025-12-29 19:22:33 -0300

    fix: resolve bg conflict

--- components/default-modal/DefaultModal.tsx
+++ components/default-modal/DefaultModal.tsx
@@ -1,9 +1,9 @@
-function Index({ children, bgBlack = false, isOpen = true }: DefaultModalProps) {
+function Index({ children, bg = false, bgBlack = false, isOpen = true }: DefaultModalProps) {
 
   if (!isOpen) return (<div />)
 
   return (
-    <div className={`h-screen flex justify-center items-center p-4 ${bgBlack && 'absolute w-full left-0 bg-black/90'}`}>
+    <div className={`h-screen flex justify-center items-center p-4 ${bg && 'bg-[#f2f2f2]'} ${bgBlack && 'absolute w-full left-0 bg-black/90'}`}>
       {children}
     </div>
   )
@@ -14,5 +14,6 @@ export default Index
 type DefaultModalProps = {
   children: any,
   bgBlack?: boolean,
-  isOpen?: boolean
+  isOpen?: boolean,
+  bg?: boolean
 }
--- components/modal-plans/ModalPlans.tsx
+++ components/modal-plans/ModalPlans.tsx
@@ -4,7 +4,7 @@ import DefaultModal from '../../components/default-modal/DefaultModal'
 
 function Index() {
 	return (
-		<DefaultModal>
+		<DefaultModal bg={true}>
 			<div className="bg-white p-6 rounded-sm shadow-md text-center w-full min-w-80 max-w-140">
 				<p className="py-2 font-light">Escolha seu perfil</p>
 				<h1 className="text-2xl font-bold text-default-purple sm:text-3xl">Buscar planos para:</h1>
